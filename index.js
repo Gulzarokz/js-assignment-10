004088865b1f814dca37ccd7cc601470934380bc
Gulzarokz <gokz22301@gamil.com>
2024-10-24 16:45:34 +0500

    Done find string and segment and spacific location

---
+++ index.js
@@ -0,0 +1,100 @@
+// 23 :Strings: Finding segments
+
+
+// first indexOf
+
+// const book = "positive thinking";
+// const indexofI = book.indexOf("i");
+// console.log(indexofI);
+
+// const book = "Quick fox jump over a lazy dog";
+// console.log(book.indexOf("f"));
+
+// const book = "Quick fox jump over a lazy dog";
+
+// const indexOfOver = book.indexOf("over");
+
+// console.log(indexOfOver);
+
+
+// lastIndexOf
+
+// const city = "hillo";
+// const lastIndexOfL = city.lastIndexOf("l");
+// console.log(lastIndexOfL);
+
+// Real world problems
+
+// // userEmail validation
+//  const userInput = prompt("Enter email");
+//  let isValid = false;
+
+//  for(let charIndex = 0; charIndex < userInput.length; charIndex++){
+//     if(userInput[charIndex] === "@"){
+//         isValid = true;
+//         break;
+//     }
+//  }
+// if(isValid){
+//     console.log("Valid email");
+    
+// }else{
+//     console.log("Invalid email");
+    
+// }
+
+// const email = prompt("Enter Email first please");
+
+// console.log(email.indexOf("@"));
+// console.log("@gamil com");
+
+// const email = prompt("Enter email first please");
+// let isValid = email.indexOf("@");
+// if(isValid === -1){
+//     console.log("invalid");
+    
+// }else{
+//     console.log("valid");
+    
+// }
+
+// Through array we can find index
+
+// const village = ["streetA", "streetB", "StreetA", "StreetD", "StreetE"];
+// // console.log(village.indexOf("StreetD"));
+// console.log(village.lastIndexOf("StreetA"));
+
+
+//24 Strings: Finding a character at a location
+
+
+// const institute = "Saylani Mass it traing";
+// const indexOfChar = institute.charAt(10);
+// console.log(institute);
+
+// console.log(indexOfChar);
+
+// const institute = "islamia college peshawar";
+// console.log(institute)
+
+// console.log(institute.charAt(12).toUpperCase());
+
+
+// const userEmail = prompt("Enter a Email please");
+// let isValid = false;
+
+// for (let charIndex = 0; charIndex < userEmail.length; charIndex++){
+//  if(userEmail.charAt([charIndex]) === "@"){
+//     isValid = true;
+//     break;
+//  }
+// }
+// if(isValid){
+//     console.log("valid Email");
+    
+// }else{
+//     console.log("Invaild Email");
+    
+// }
+
+ 
